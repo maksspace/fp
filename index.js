6c8de5214a4176999bd9b0133838e5f55aa6f99a
maksspace <maksspaceworld@yandex.ru>
2017-08-26 23:02:13 +0300

    add error handling in promises chain. added comments

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-import {map, compose, sum} from 'lodash/fp';
+import {map, compose, sum, filter} from 'lodash/fp';
 import axios from 'axios';
 
 const fetchByURL = url => axios.get(url);
 
-const convert = base => rates => 
-  rates.reduce((total, item) => {
-    return {
-      ...total,
-      [item.code]: item.rate * base
-    };
-  }, {});
-
+// Собираем URL для для запросы курса валют (currencies) относительно (base)
 const exchangeURL = (base) => (currencies) => {
   return `http://api.fixer.io/latest?base=${base}&symbols=${currencies}`;
 };
 
+// Преобразуем обьект с ответом от сервера, в массив обьектов вида:
+// [ { code: Код валюты, rate: курс }, ... ]
+// Курс базовой балюты относительно себя === 1
 const parseExchangeRates = res => {
   const { rates } = res.data;
   const ratesArray = Object.keys(rates).map(key => {
@@ -27,16 +23,35 @@ const parseExchangeRates = res => {
   ];
 };
 
+const convert = base => rates => {
+  return rates.reduce((total, item) => {
+    return {
+      ...total,
+      [item.code]: item.rate * base
+    };
+  }, {});
+};
+
 const itemPrice = item => item.price;
+
+// Заправшиваем курс валют относительно доллара
 const fetchUSDExchangeRates = compose(
   fetchByURL,
   exchangeURL('USD')
 );
+
+// Считаем общую стоимость товаров в корзине.
+// Базовая валюта - USD
 const cartTotal = compose(
   sum,
   map(itemPrice)
 );
 
+// Выводим в консоль ошибки
+const handleError = e => console.log(`Error: ${e.message}`)
+
+// Список валют, в которые надо перевести базовую
+// для простоты - базовая тоже входит в этот список
 const currencies = [
   'USD',
   'RUB',
@@ -45,6 +60,7 @@ const currencies = [
   'JPY',
 ];
 
+// Козина товаров
 const selectedCart = [
 	{ price: 20 },
 	{ price: 45 },
@@ -52,7 +68,21 @@ const selectedCart = [
 	{ price: 1305 }
 ];
 
+// Считаем общую стоимость корзины товаров, в разных валютах
+const exchangedTotalCartPrice = compose(
+  convert,
+  cartTotal
+);
+
+// Получаем курсы валют относительно доллара
 fetchUSDExchangeRates(currencies)
+  // Парсим курсы в нужный нам вид
   .then(parseExchangeRates)
-  .then(convert(cartTotal(selectedCart)))
-  .then(console.log)
+  // Считаем общую стоимоть корзины в разных валютах
+  .then(exchangedTotalCartPrice(selectedCart))
+  // Просто печатаем в консоль что получилось
+  .then(totalCartPrice => {
+    console.log('totalCartPrice: ', totalCartPrice);
+  })
+  // Пишем ошибку в консоль (если она есть)
+  .catch(handleError)
