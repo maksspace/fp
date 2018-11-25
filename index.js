809d133fa3c41e1ce9d278f5c477267cc47ecf55
Maks Smagin <maksspaceworld@yandex.ru>
2018-11-25 14:18:33 +0300

    remove russian comments

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import axios from 'axios';
 
 const fetchByURL = url => axios.get(url);
 
-// Собираем URL для запроса курса валют (currencies) относительно (base)
+// URL exchange rates relative to the base
 const exchangeURL = (base) => (currencies) => {
   return `http://api.fixer.io/latest?base=${base}&symbols=${currencies}`;
 };
 
-// Преобразуем обьект с ответом от сервера, в массив обьектов вида:
-// [ { code: Код валюты, rate: курс }, ... ]
-// Курс базовой балюты относительно себя === 1
+// [ { code: cyrrency code, rate: rate }, ... ]
+// Exchanges rate base currency relative to itself === 1
 const parseExchangeRates = res => {
   const { rates } = res.data;
   const ratesArray = Object.keys(rates).map(key => {
@@ -34,24 +33,19 @@ const convert = base => rates => {
 
 const itemPrice = item => item.price;
 
-// Заправшиваем курс валют относительно доллара
 const fetchUSDExchangeRates = compose(
   fetchByURL,
   exchangeURL('USD')
 );
 
-// Считаем общую стоимость товаров в корзине.
-// Базовая валюта - USD
+// USD - base currency
 const cartTotal = compose(
   sum,
   map(itemPrice)
 );
 
-// Выводим в консоль ошибку
 const handleError = e => console.log(`Error: ${e.message}`)
 
-// Список валют, в которые надо перевести базовую
-// для простоты - базовая тоже входит в этот список
 const currencies = [
   'USD',
   'RUB',
@@ -60,27 +54,20 @@ const currencies = [
   'JPY',
 ];
 
-// Козина товаров
 const selectedCart = [
-	{ price: 20 },
-	{ price: 45 },
-	{ price: 67 },
-	{ price: 1305 }
+  { price: 20 },
+  { price: 45 },
+  { price: 67 },
+  { price: 1305 }
 ];
 
-// Считаем общую стоимость корзины товаров, в разных валютах
 const exchangedTotalCartPrice = compose(
   convert,
   cartTotal
 );
 
-// Получаем курсы валют относительно доллара
 fetchUSDExchangeRates(currencies)
-  // Парсим курсы в нужный нам вид
   .then(parseExchangeRates)
-  // Считаем общую стоимоть корзины в разных валютах
   .then(exchangedTotalCartPrice(selectedCart))
-  // Просто печатаем в консоль что получилось
   .then(console.log)
-  // Пишем ошибку в консоль (если она есть)
   .catch(handleError)
